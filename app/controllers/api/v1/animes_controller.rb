de1ed3a3cb975554e5a5ffc0a3e0149b0f5e20be
pablopawpaw <lpault29@gmail.com>
2018-07-10 11:35:39 -0400

    fetch from backend works

--- app/controllers/api/v1/animes_controller.rb
+++ app/controllers/api/v1/animes_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::AnimesController < ApplicationController
 
+  def self.fetch
+    # baseUrl = "https://api.jikan.moe/search"
+    # type = type # anime, manga, person, character, season
+    # query = query # q=Fate/Zero
+    # page = "&page=1" # &page=1
+    # response = RestClient.get("#{baseUrl}/#{type}/q=#{query}/#{page}")
+    result = JSON.parse(RestClient.get("https://api.jikan.moe/search/anime?q=Fate/Zero&page=1
+"))
+    puts result["result"][0]["title"]
+  end
+
   def index
     @animes = Anime.all
   end
